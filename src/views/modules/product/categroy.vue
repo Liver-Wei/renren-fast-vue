f257cf0f398a076836bd16608ac7e8756e72920c
Liver-Wei <1078747843@qq.com>
2022-10-31 22:30:42 +0800

    品牌管理图片上传完成

--- src/views/modules/product/categroy.vue
+++ src/views/modules/product/categroy.vue
@@ -1,41 +1,96 @@
 <template>
-  <el-tree
-    :data="data"
-    :props="defaultProps"
-    :expand-on-click-node="false"
-    :default-expanded-keys = "expandKey"
-    show-checkbox
-    node-key="catId"
-    ><span class="custom-tree-node" slot-scope="{ node, data }">
-      <span>{{ node.label }}</span>
-      <span>
-        <el-button
-          v-if="node.level <= 2"
-          type="text"
-          size="mini"
-          @click="() => append(data)"
-        >
-          Append
-        </el-button>
-        <el-button
-          v-if="node.childNodes.length == 0"
-          type="text"
-          size="mini"
-          @click="() => remove(node, data)"
-        >
-          Delete
-        </el-button>
+  <div>
+    <el-button type="danger" @click="batchDelete" round>批量删除</el-button>
+    <el-tree
+      :data="data"
+      :props="defaultProps"
+      :expand-on-click-node="false"
+      :default-expanded-keys="expandKey"
+      ref="menuTree"
+      show-checkbox
+      node-key="catId"
+      ><span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)"
+          >
+            添加
+          </el-button>
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => edit(data)"
+          >
+            修改
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >
+            删除
+          </el-button>
+        </span>
+      </span></el-tree
+    >
+    <el-dialog
+      :title="title"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :close-on-click-modal="false"
+    >
+      <el-form :model="category">
+        <el-form-item label="分类名称">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <el-form :model="category">
+        <el-form-item label="图标">
+          <el-input v-model="category.icon" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <el-form :model="category">
+        <el-form-item label="计量单位">
+          <el-input
+            v-model="category.productUnit"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitData">确 定</el-button>
       </span>
-    </span></el-tree
-  >
+    </el-dialog>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      title: "",
+      category: {
+        icon: "",
+        productUnit: "",
+
+        name: "",
+        parentCid: 0,
+        catLevel: 0,
+        showStatus: 1,
+        sort: 0,
+        catId: null,
+        dialogType: ""
+      },
+      dialogVisible: false,
       data: [],
-      expandKey:[],
+      expandKey: [],
       defaultProps: {
         children: "children",
         label: "name"
@@ -43,6 +98,35 @@ export default {
     };
   },
   methods: {
+    batchDelete(){
+      let checkKeys = this.$refs.menuTree.getCheckedKeys();
+      console.log("选中的节点",checkKeys);
+      this.$confirm(`是否批量删除菜单?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          this.$http({
+            url: this.$http.adornUrl("/product/category/delete"),
+            method: "post",
+            data: this.$http.adornData(checkKeys, false)
+          }).then(() => {
+            console.log("删除成功...");
+            this.$message({
+              type: "success",
+              message: `删除成功!`
+            });
+            this.getmenus();
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    },
     getmenus() {
       this.$http({
         url: this.$http.adornUrl("/product/category/list/tree"),
@@ -54,6 +138,75 @@ export default {
     },
     append(data) {
       console.log("append", data);
+      (this.title = "添加分类"),
+        (this.category.dialogType = "add"),
+        (this.dialogVisible = true);
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel * 1 + 1;
+      this.category.name = "";
+      this.category.catId = null;
+      this.category.icon = "";
+      this.category.productUnit = "";
+      this.category.sort = 0;
+      this.category.showStatus = 1;
+    },
+    edit(data) {
+      console.log("要修改的数据：", data);
+      (this.title = "修改分类"), (this.category.dialogType = "edit");
+      this.dialogVisible = true;
+
+      //获取当前节点最新数据
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: "get"
+      }).then(({ data }) => {
+        this.category.name = data.data.name;
+        this.category.catId = data.data.catId;
+        this.category.icon = data.data.icon;
+        this.category.productUnit = data.data.productUnit;
+        this.category.parentCid = data.data.parentCid;
+        console.log("------------------");
+        console.log("查看回调数据：", data);
+      });
+    },
+    submitData() {
+      if (this.category.dialogType == "add") {
+        this.addCategory();
+      } else {
+        this.editCategory();
+      }
+    },
+    editCategory() {
+      var { name, catId, icon, productUnit } = this.category;
+      this.$http({
+        url: this.$http.adornUrl("/product/category/update"),
+        method: "post",
+        data: this.$http.adornData({ name, catId, icon, productUnit }, false)
+      }).then(({ data }) => {
+        this.$message({
+          type: "success",
+          message: "菜单修改成功！"
+        });
+        this.dialogVisible = false;
+        this.getmenus();
+        this.expandKey = [this.category.parentCid];
+      });
+    },
+    addCategory() {
+      console.log("提交的数据：", this.category);
+      this.$http({
+        url: this.$http.adornUrl("/product/category/save"),
+        method: "post",
+        data: this.$http.adornData(this.category, false)
+      }).then(({ data }) => {
+        this.$message({
+          type: "success",
+          message: "菜单保存成功！"
+        });
+        this.dialogVisible = false;
+        this.getmenus();
+        this.expandKey = [this.category.parentCid];
+      });
     },
     remove(node, data) {
       let ids = [data.catId];
@@ -74,7 +227,7 @@ export default {
               message: `${data.name}删除成功!`
             });
             this.getmenus();
-            this.expandKey=[node.parent.data.catId];
+            this.expandKey = [node.parent.data.catId];
           });
         })
         .catch(() => {
